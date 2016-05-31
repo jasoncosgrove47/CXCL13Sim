a1ca958d91ca891857dd0a4ad24df4fa2f575de1
Jason Cosgrove <jc1571@nas10-240-33-122.york.ac.uk>
2016-05-31 15:47:27 +0100

    Added a method to calculate turning angle

--- dataLogger/ProcessData.java
+++ dataLogger/ProcessData.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import sim.util.Double3D;
+import sim3d.util.Vector3DHelper;
 
 public class ProcessData {
 
@@ -70,15 +71,12 @@ public class ProcessData {
 		// calculate the total time
 		double time = Xcoords.size();
 
-		
-		netDisplacement = calculateNetDisplacement(startLocation,endLocation);
-		
-		double meanderingIndex = calculateMeanderingIndex(totalDisplacement,netDisplacement, time);
-		
-		double motilityCoefficient = calculateMotilityCoefficient(netDisplacement, time);
-		
+		// calculate the net displacement travelled
+		netDisplacement = startLocation.distance(endLocation);
+		double meanderingIndex = calculateMeanderingIndex(totalDisplacement, netDisplacement,time);
+		double motilityCoefficient = calculateMotilityCoefficient(netDisplacement,time);
 		double speed = calculateSpeed(totalDisplacement,time);
-	
+		
 		// store all motility parameters in an output array
 		double[] output = { time, motilityCoefficient, meanderingIndex, speed,
 				x, y, z };
@@ -86,14 +84,12 @@ public class ProcessData {
 		return output;
 	}
 
-	
 	/**
 	 * Calculates the speed, motility coefficient and meandering index for each
 	 * cell
 	 * 
 	 * @param key
 	 *            for an individual cell
-	 * @return a double array with relevant motility parameters
 	 * @throws IOException
 	 */
 	static void processRawData(Integer key,
@@ -105,8 +101,10 @@ public class ProcessData {
 				.get(key);
 		ArrayList<Double> Zcoords = Controller.getInstance().getZ_Coordinates()
 				.get(key);
+
 		ArrayList<Integer> Receptors = Controller.getInstance().getReceptors()
 				.get(key);
+
 
 		double x = 0, y = 0, z = 0;
 		int r = 0;
@@ -123,7 +121,6 @@ public class ProcessData {
 			r = Receptors.get(i);
 
 			// update raw data file
-
 			rawDataWriter.append(Integer.toString(key));
 			rawDataWriter.append(',');
 			rawDataWriter.append(Integer.toString(i));
@@ -137,35 +134,150 @@ public class ProcessData {
 			rawDataWriter.append(Integer.toString(r));
 			rawDataWriter.append('\n');
 
-		}
+		}	
 	}
 	
-	
-	
-	
-	private static double calculateNetDisplacement(Double3D startLocation, Double3D endLocation){
-		return startLocation.distance(endLocation);
+	/**
+	 * Calculate the motility coefficient of a cell
+	 */
+	private static double calculateMotilityCoefficient(double netDisplacement, double time){
+		
+		return (Math.pow(netDisplacement, 2) / (6 * time));
+		
 	}
 	
+	/**
+	 * Calculate the meandering index of a cell
+	 * @param totalDisplacement
+	 * @param netDisplacement
+	 * @param time
+	 * @return
+	 */
+	private static double calculateMeanderingIndex(double totalDisplacement, double netDisplacement, double time){
+		//calculate meandering index
+		double mi =  netDisplacement/totalDisplacement;
+		//correct for time dependency of tracking
+		//as discussed in analysing immune cell
+		//migration de Boer 2009
+		double miCorrected = mi*Math.sqrt(time);
+		return miCorrected;
+	}
 	
+	/**
+	 * Calculates the velocity of a cell
+	 * @param totalDisplacement total distance travelled during a tracking expt
+	 * @param time  total duration of tracking expt
+	 * @return the velocity
+	 */
 	private static double calculateSpeed(double totalDisplacement, double time){
 		return totalDisplacement/time;
 	}
 	
 	
-	private static double calculateMotilityCoefficient(double netDisplacement, double time){		
-		return (Math.pow(netDisplacement, 2) / (6 * time));
-	}
+	/**
+	 * Calculate turning angle: taken from matlab script
+	 * provided by Jens. The angle between two 3D vectors
+	 * is given by the acos of the dot product of two
+	 * normalised vectors. 
+	 * 
+	 * To determine directionality we first determine the 
+	 * axis of rotation given by the cross-product of the two
+	 * vectors. A positive cross-product along the axis is
+	 * counter clockwise and so we call this a negative turn
+	 * 
+	 */
+	private double calculateTurningAngle(Double3D p1, Double3D p2 , Double3D p3){
+		
+		//calculate vector1 and vector 2 from the three points
+		
+		double v1_x,v1_y,v1_z,v2_x,v2_y,v2_z;
+		
+		v1_x = p1.x - p2.x;
+		v1_y = p1.y - p2.y;
+		v1_z = p1.z - p2.z;
+		
+		v2_x = p2.x - p3.x;
+		v2_y = p2.y - p3.y;
+		v2_z = p2.z - p3.z;
+		
+		Double3D v1 = new Double3D(v1_x,v1_y,v1_z);
+		Double3D v2 = new Double3D(v2_x,v2_y,v2_z);
+		
+
+		
+		double angleRadians = (Math.acos(Vector3DHelper.dotProduct(v1.normalize(), v2.normalize())));
+		//convert into degrees
+		double angleDegrees = Math.toDegrees(angleRadians);
 	
-	
-	private static double calculateMeanderingIndex(double totalDisplacement,double netDisplacement, double time){
-		return (netDisplacement / totalDisplacement)
-		* Math.sqrt(time);
+		
+		//now we need the z component of the vector
+		Double3D crossProduct = Vector3DHelper.crossProduct(v1, v2);
+		
+		// A positive crossproduct along z ( the sign of the
+		// z component) is counter clockwise. We call this a negative turn
+		// so reverse the sign on the angle
+		if(crossProduct.z > 0){
+			return - angleDegrees;
+		}
+		else{
+			return angleDegrees;
+		}
+		
+		
+		
 		
 	}
 	
+	/*
+	* Calculate the angle between two 3D vectors
+	*
+	* the angle between two vectors P and Q is:
+	* arcos((P.Q)/(|P||Q|))
+	* 
+	* theta <- acos( sum(a*b) / ( sqrt(sum(a * a)) * sqrt(sum(b * b)) ) )
+	* 
+	* the cross product then gives you the direction along the Z axis
+	* (the z-component of the Double3D). A positive angle means you are 
+	* going anti-clockwise (right hand rule) so we call this a negative angle
+	* 
+	* 
+	*/
 
-	
+	static double calculateTurningAngle2(Double3D p1,Double3D p2,Double3D p3 ){
+		
+		double v1_x,v1_y,v1_z,v2_x,v2_y,v2_z;
+		
+	    //calculate vector 1 and vector 2 from p1,p2 and p3
+	    v1_x = (p1.x - p2.x);
+	    v1_y = (p1.y - p2.y);
+	    v1_z = (p1.z - p2.z);
+	    
+	    v2_x = (p2.x - p3.x);
+	    v2_y = (p2.y - p3.y);
+	    v2_z = (p2.z - p3.z);
+		
+		Double3D v1 = new Double3D(v1_x,v1_y,v1_z);
+		Double3D v2 = new Double3D(v2_x,v2_y,v2_z);
+		
+		
+		// the angle between two vectors P and Q is:
+		// arcos((P.Q)/(|P||Q|))
+		double angle = Math.acos(Vector3DHelper.dotProduct(v1.normalize(), v2.normalize()));
+		
+		// the cross product then gives you the direction along the Z axis
+		// (the z-component of the Double3D). A positive angle means you are 
+		// going anti-clockwise (right hand rule) so we call this a negative angle
+		Double3D crossproduct = Vector3DHelper.crossProduct(v1, v2);
+		
+		if(crossproduct.z > 0){
+			return -angle;
+		}
+		
+		else{
+			return angle;
+		}
+		
+	}
 	
 	
 }
