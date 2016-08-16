ee7a96b11dfcd73a0017bcbfe312c7d0816306dd
Jason Cosgrove <jc1571@york.ac.uk>
2016-08-16 15:20:06 +0100

    Added in tests for the TC class

--- integrationtests/SystemTests.java
+++ integrationtests/SystemTests.java
@@ -31,8 +31,6 @@ public class SystemTests {
 	public void testShouldAcquireAntigen() {
 		// set up the simulation
 		
-		
-		
 		long steps = 0;
 		long seed = System.currentTimeMillis();
 		SimulationEnvironment sim = new SimulationEnvironment(
